1d03d90f8fd34cf5da3497d5399a5242632bbcea
ucyo <cayoglu@me.com>
2021-12-29 09:40:49 +0100

    Add configuration class for flask

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+
+class Config:
+    DEBUG = True
+    SECRET_KEY = "secret"
+
 app = Flask(__name__)
+app.config.from_object("app.Config")
 
 @app.route("/")
 def hello_world():

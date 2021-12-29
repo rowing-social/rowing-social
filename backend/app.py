64518e55ec3411c5f6cc3e8315c32c1ffbabd038
ucyo <cayoglu@me.com>
2021-12-29 11:30:01 +0100

    Add Users API for backend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from models import db
+from flask import Flask, jsonify, request
+from models import db, User
+from sqlalchemy.exc import SQLAlchemyError
 
 app = Flask(__name__)
 app.config.from_object("config.Config")
@@ -10,3 +11,33 @@ db.create_all()
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route("/users", methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users = [{"id": x.id, "name":x.name, "email":x.email} for x in users]
+    return jsonify(users)
+
+@app.route("/users", methods=['POST'])
+def post_users():
+    data = request.get_json()
+    try:
+        name = data['name']
+        email = data['email']
+    except:
+        return jsonify({'success': False}), 400
+    user = User(name=name, email=email)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({"success": False}), 420
+    return jsonify({"success": True}), 201
+
+@app.route("/users/<user_id>", methods=['GET'])
+def get_user_by_id(user_id):
+    user = User.query.filter_by(id=user_id).first_or_404()
+    user = {"id": user.id, "name": user.name, "email": user.email}
+    return jsonify(user)
